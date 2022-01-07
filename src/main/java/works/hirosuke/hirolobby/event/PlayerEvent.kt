d9546f8c5e521ef0d8ae8c02b5ee6fb82f51d7f3
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-01-08 05:57:05 +0900

    add: velocity plate

--- src/main/java/works/hirosuke/hirolobby/event/PlayerEvent.kt
+++ src/main/java/works/hirosuke/hirolobby/event/PlayerEvent.kt
@@ -1,8 +1,12 @@
 package works.hirosuke.hirolobby.event
 
+import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import works.hirosuke.hirolobby.HiroData
@@ -25,6 +29,8 @@ class PlayerEvent: Listener {
             player.teleport(HiroData.spawnLocation)
         }
 
+        player.isFlying = false
+
         ScoreboardUtil.update()
     }
 
@@ -32,6 +38,17 @@ class PlayerEvent: Listener {
     fun on(e: PlayerQuitEvent) {
         lobby.runTaskLater(1) {
             ScoreboardUtil.update()
+        }
+    }
+
+    @EventHandler
+    fun on(e: PlayerInteractEvent) {
+        val player = e.player
+        val block = e.clickedBlock
+
+        if (e.action == Action.PHYSICAL && block != null && block.type == Material.HEAVY_WEIGHTED_PRESSURE_PLATE) {
+            player.velocity = player.location.direction.multiply(3).setY(1)
+            player.playSound(player.location, Sound.ENTITY_WITHER_SHOOT, 0.5f, 1f)
         }
     }
 }

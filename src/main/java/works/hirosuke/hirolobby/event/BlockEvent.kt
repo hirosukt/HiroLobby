3c0a0a070094afec7f72b04e4df5666e4cf492d0
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-01-28 00:43:43 +0900

    remove: fade canceling event

--- src/main/java/works/hirosuke/hirolobby/event/BlockEvent.kt
+++ src/main/java/works/hirosuke/hirolobby/event/BlockEvent.kt
@@ -2,18 +2,12 @@ package works.hirosuke.hirolobby.event
 
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockFadeEvent
 import org.bukkit.event.block.LeavesDecayEvent
 
 class BlockEvent: Listener {
 
     @EventHandler
     fun on(e: LeavesDecayEvent) {
-        e.isCancelled = true
-    }
-
-    @EventHandler
-    fun on(e: BlockFadeEvent) {
         e.isCancelled = true
     }
 }
